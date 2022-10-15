48bbca5c4b1154f6847ac72ea3bc54c642e28169
Jayanth <101553315+GandhodiJayanth@users.noreply.github.com>
2022-10-16 05:15:50 +0530

    without animation code (#1)
    Co-authored-by: bachelorskitchen108 <bachelorskitchen108@gmail.com>

---
+++ new-react-portfolio/src/Components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Navbar, Nav } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
+
+const Header = () => {
+    let activeStyle = {
+        color: '#deb04c'
+    }
+  return (
+    <div className='header'>
+        <Navbar expand="lg" fixed="top">
+            <Navbar.Brand>Gandhodi Jayanth</Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse className='justify-content-end'>
+                <Nav className="ml-auto">
+                     <NavLink to="/" className="nav-link" style={({isActive}) => (isActive ? activeStyle : undefined)}>Home</NavLink>
+                     <NavLink to="/projects" className="nav-link" style={({isActive}) => (isActive ? activeStyle : undefined)}>Projects</NavLink>
+                     <NavLink to="/services" className="nav-link" style={({isActive}) => (isActive ? activeStyle : undefined)}>Services</NavLink>
+                     <NavLink to="/about" className="nav-link" style={({isActive}) => (isActive ? activeStyle : undefined)}>About</NavLink>
+                     <NavLink to="/contact" className="nav-link" style={({isActive}) => (isActive ? activeStyle : undefined)}>Contact</NavLink>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    </div>
+  )
+}
+
+export default Header
